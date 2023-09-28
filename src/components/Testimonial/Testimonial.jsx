c504521a01c811283f14ffca37aad720f86ba4c9
Hamza Yaseen <hamzza.yaseen@cooperativecomputing.com>
2023-09-28 16:30:41 +0500

    fix: fix home page content

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -6,7 +6,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import patientAvatar from "../../assets/images/patient-avatar.png";
+import patientAvatar from "../../assets/images/patient.png";
 import { HiStar } from "react-icons/hi";
 
 const Testimonial = () => {
@@ -36,10 +36,10 @@ const Testimonial = () => {
         <SwiperSlide>
           <div className="py-[30px] px-5 rounded-3 ">
             <div className="flex items-center gap-[13px]">
-              <img src={patientAvatar} alt="" />
+              <img src={patientAvatar} alt="" className="w-20 h-20 "/>
               <div>
                 <h4 className="text-[18px] leading-[30px] text-headingColor font-[600]">
-                  Muhibur Rahman
+                  Haider
                 </h4>
                 <div className="flex items-center gap-[2px]">
                   <HiStar className="text-yellowColor w-[18px] h-5" />
@@ -60,10 +60,10 @@ const Testimonial = () => {
         <SwiperSlide>
           <div className="py-[30px] px-5 rounded-3 ">
             <div className="flex items-center gap-[13px]">
-              <img src={patientAvatar} alt="" />
+              <img src={patientAvatar} alt="" className="w-20 h-20 "/>
               <div>
                 <h4 className="text-[18px] leading-[30px] text-headingColor font-[600]">
-                  Muhibur Rahman
+                 Amina Ali
                 </h4>
                 <div className="flex items-center gap-[2px]">
                   <HiStar className="text-yellowColor w-[18px] h-5" />
@@ -76,18 +76,19 @@ const Testimonial = () => {
             </div>
 
             <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
-              “I have taken medical services from them. They treat so well and
-              they are provding the best medical services.”
+            “This app has made healthcare management incredibly convenient. 
+            I highly recommend it to anyone seeking efficient medical services.”
+
             </p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="py-[30px] px-5 rounded-3 ">
             <div className="flex items-center gap-[13px]">
-              <img src={patientAvatar} alt="" />
+              <img src={patientAvatar} alt="" className="w-20 h-20 "/>
               <div>
                 <h4 className="text-[18px] leading-[30px] text-headingColor font-[600]">
-                  Muhibur Rahman
+                  Ahmed Malik
                 </h4>
                 <div className="flex items-center gap-[2px]">
                   <HiStar className="text-yellowColor w-[18px] h-5" />
@@ -100,18 +101,20 @@ const Testimonial = () => {
             </div>
 
             <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
-              “I have taken medical services from them. They treat so well and
-              they are provding the best medical services.”
+            “I've been using this app for a while now, and it never disappoints. 
+            The healthcare providers are excellent, and I appreciate the ease of 
+            scheduling appointments.”
+
             </p>
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="py-[30px] px-5 rounded-3 ">
             <div className="flex items-center gap-[13px]">
-              <img src={patientAvatar} alt="" />
+              <img src={patientAvatar} alt="" className="w-20 h-20 " />
               <div>
                 <h4 className="text-[18px] leading-[30px] text-headingColor font-[600]">
-                  Muhibur Rahman
+                  Ayesha Khan
                 </h4>
                 <div className="flex items-center gap-[2px]">
                   <HiStar className="text-yellowColor w-[18px] h-5" />
@@ -124,11 +127,42 @@ const Testimonial = () => {
             </div>
 
             <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
-              “I have taken medical services from them. They treat so well and
-              they are provding the best medical services.”
+              “Booking appointments through this app is a breeze, and the healthcare 
+              professionals are highly skilled and caring. I'm grateful for the 
+              convenience it offers!”
             </p>
           </div>
         </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="py-[30px] px-5 rounded-3 ">
+            <div className="flex items-center gap-[13px]">
+              <img src={patientAvatar} alt="" className="w-20 h-20 "/>
+              <div>
+                <h4 className="text-[18px] leading-[30px] text-headingColor font-[600]">
+                  Sara Ahmeed
+                </h4>
+                <div className="flex items-center gap-[2px]">
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                </div>
+              </div>
+            </div>
+
+            <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+            “I've had a wonderful experience using this app. It's incredibly user-friendly, 
+            and the healthcare professionals are both skilled and compassionate. 
+            I trust this app with my medical needs.”
+
+
+            </p>
+          </div>
+        </SwiperSlide>
+
+        
       </Swiper>
     </div>
   );
