ba701e0ba21400e30c2d4a2db7b0f1b36f274235
Hamza Yaseen <hamzza.yaseen@cooperativecomputing.com>
2023-10-13 13:03:47 +0500

    made changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
-import heroImg01 from "../assets/images/hero-img01.png";
-import heroImg02 from "../assets/images/hero-img02.png";
-import heroImg03 from "../assets/images/hero-img03.png";
+import heroImg01 from "../assets/images/local1.png";
+import heroImg02 from "../assets/images/local2.avif";
+import heroImg03 from "../assets/images/local3.jpeg";
 import icon01 from "../assets/images/icon01.png";
 import icon02 from "../assets/images/icon02.png";
 import icon03 from "../assets/images/icon03.png";
