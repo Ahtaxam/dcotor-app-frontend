6819db074b2c84d9dfb001851e07e491a92b192e
Awon Abbas <awonabbas2@gmail.com>
2023-10-13 13:14:56 +0500

    Added female avatar for reviews

--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -7,6 +7,7 @@ import "swiper/css";
 import "swiper/css/pagination";
 
 import patientAvatar from "../../assets/images/patient.png";
+import femalePatientAvatar from "../../assets/images/femalePatient.png";
 import { HiStar } from "react-icons/hi";
 
 const Testimonial = () => {
@@ -36,10 +37,10 @@ const Testimonial = () => {
         <SwiperSlide>
           <div className="py-[30px] px-5 rounded-3 ">
             <div className="flex items-center gap-[13px]">
-              <img src={patientAvatar} alt="" className="w-20 h-20 "/>
+              <img src={femalePatientAvatar} alt="" className="w-20 h-20 "/>
               <div>
                 <h4 className="text-[18px] leading-[30px] text-headingColor font-[600]">
-                  Haider
+                  Amina Ali
                 </h4>
                 <div className="flex items-center gap-[2px]">
                   <HiStar className="text-yellowColor w-[18px] h-5" />
@@ -63,7 +64,7 @@ const Testimonial = () => {
               <img src={patientAvatar} alt="" className="w-20 h-20 "/>
               <div>
                 <h4 className="text-[18px] leading-[30px] text-headingColor font-[600]">
-                 Amina Ali
+                 Haider
                 </h4>
                 <div className="flex items-center gap-[2px]">
                   <HiStar className="text-yellowColor w-[18px] h-5" />
@@ -111,7 +112,7 @@ const Testimonial = () => {
         <SwiperSlide>
           <div className="py-[30px] px-5 rounded-3 ">
             <div className="flex items-center gap-[13px]">
-              <img src={patientAvatar} alt="" className="w-20 h-20 " />
+              <img src={femalePatientAvatar} alt="" className="w-20 h-20 " />
               <div>
                 <h4 className="text-[18px] leading-[30px] text-headingColor font-[600]">
                   Ayesha Khan
@@ -137,10 +138,10 @@ const Testimonial = () => {
         <SwiperSlide>
           <div className="py-[30px] px-5 rounded-3 ">
             <div className="flex items-center gap-[13px]">
-              <img src={patientAvatar} alt="" className="w-20 h-20 "/>
+              <img src={femalePatientAvatar} alt="" className="w-20 h-20 "/>
               <div>
                 <h4 className="text-[18px] leading-[30px] text-headingColor font-[600]">
-                  Sara Ahmeed
+                  Sara Ahmed
                 </h4>
                 <div className="flex items-center gap-[2px]">
                   <HiStar className="text-yellowColor w-[18px] h-5" />
